8f51ed4977c97656dbd1f1d7ac5a5f7b4417955b
Xaltra\yaron <yaron.walter@gmail.com>
2019-05-08 19:04:11 +0200

    Kategorien-Mock

--- src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
+++ src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
@@ -3,6 +3,8 @@ package ch.bfh.bti7081.model.manager;
 import ch.bfh.bti7081.model.seminar.Seminar;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,22 +13,14 @@ public class SeminarManager {
     // manages the communication between backend and frontend
     // the list of methods isn't completed yet, just some sample methods for the class diagramm
 
-    public List<Seminar> getSeminars() {
-        throw new NotImplementedException();
+    public static List<Seminar> getSeminaries() {
+        return mockSeminaries();
     }
 
     public List<Seminar> getFilteredSeminars(Map<String, String> filters) {
         throw new NotImplementedException();
     }
 
-    public List<SeminarCategory> getCategories(){
-        List<SeminarCategory> mockSeminaries = new ArrayList<>();
-        mockSeminaries.add(new SeminarCategory("Betroffene helfen Betroffenen"));
-        mockSeminaries.add(new SeminarCategory("Angehörige erzählen"));
-        mockSeminaries.add(new SeminarCategory("Ärzte geben Auskunft"));
-        mockSeminaries.add(new SeminarCategory("Allgemeines"));
-        return mockSeminaries;
-    }
     public Seminar createSeminar(Seminar seminar) {
         throw new NotImplementedException();
     }
