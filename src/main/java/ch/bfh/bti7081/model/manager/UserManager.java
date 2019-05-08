dc4455d026f217c59a448be910cb5875720e7322
Xaltra\yaron <yaron.walter@gmail.com>
2019-05-08 19:03:12 +0200

    Seminar-Mocks hochgeladen

---
+++ src/main/java/ch/bfh/bti7081/model/manager/UserManager.java
@@ -0,0 +1,13 @@
+package ch.bfh.bti7081.model.manager;
+
+import ch.bfh.bti7081.model.seminar.Seminar;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class UserManager {
+    public static List<Seminar> getUserById(Integer id) {
+        throw new NotImplementedException();
+    }
+
+}
