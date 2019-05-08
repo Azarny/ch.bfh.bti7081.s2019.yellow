a9d8eb1e272a9e6a4bb57ad593cdd167d77091d4
Xaltra\yaron <yaron.walter@gmail.com>
2019-05-08 17:16:21 +0200

    Kategorien-Mock

--- src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
+++ src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
@@ -1,8 +1,10 @@
 package ch.bfh.bti7081.model.manager;
 
 import ch.bfh.bti7081.model.seminar.Seminar;
+import ch.bfh.bti7081.model.seminar.SeminarCategory;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +20,14 @@ public class SeminarManager {
         throw new NotImplementedException();
     }
 
+    public List<SeminarCategory> getCategories(){
+        List<SeminarCategory> mockSeminaries = new ArrayList<>();
+        mockSeminaries.add(new SeminarCategory("Betroffene helfen Betroffenen"));
+        mockSeminaries.add(new SeminarCategory("Angehörige erzählen"));
+        mockSeminaries.add(new SeminarCategory("Ärzte geben Auskunft"));
+        mockSeminaries.add(new SeminarCategory("Allgemeines"));
+        return mockSeminaries;
+    }
     public Seminar createSeminar(Seminar seminar) {
         throw new NotImplementedException();
     }
