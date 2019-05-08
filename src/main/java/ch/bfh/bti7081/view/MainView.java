97766fb535ffe93dc1676dff470be93ade5daddb
Lucien Heuzeveldt <lucien.heuzeveldt@gmail.com>
2019-05-08 17:24:13 +0200

    add layout and navigation

--- src/main/java/ch/bfh/bti7081/view/MainView.java
+++ src/main/java/ch/bfh/bti7081/view/MainView.java
@@ -1,7 +1,11 @@
 package ch.bfh.bti7081.view;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
@@ -9,13 +13,12 @@ import com.vaadin.flow.server.PWA;
 /**
  * The main view contains a button and a click listener.
  */
-@Route("")
+@Route(value = "", layout = Layout.class)
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
+    private H1 title = new H1("Herzlich Willkommen!");
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        this.add(title);
     }
 }
