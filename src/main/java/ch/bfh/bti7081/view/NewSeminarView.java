96831fc7bb2fba3a38c035a5c9ecd9592b37bb16
Xaltra\yaron <yaron.walter@gmail.com>
2019-05-12 15:55:39 +0200

    overall layout but with unfinished binding

--- src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
+++ src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
@@ -1,58 +1,102 @@
 package ch.bfh.bti7081.view;
 
+import ch.bfh.bti7081.model.manager.SeminarCategoryManager;
 import ch.bfh.bti7081.model.manager.SeminarManager;
 import ch.bfh.bti7081.model.seminar.Seminar;
-import com.vaadin.flow.component.datepicker.DatePicker;
+import ch.bfh.bti7081.model.seminar.SeminarCategory;
+import ch.bfh.bti7081.view.single_components.LocalDateTimePicker;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.BinderValidationStatus;
 import com.vaadin.flow.data.binder.BindingValidationStatus;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Route(value = "newSeminar", layout = Layout.class)
-public class NewSeminarView extends FormLayout {
+public class NewSeminarView extends VerticalLayout {
+
     private H1 title = new H1("Seminar erstellen");
 
-    //FormLayout layoutWithBinder = new FormLayout();
-    Binder<Seminar> binder = new Binder<>();
-
-    // The object that will be edited
-    Seminar newSeminar = new Seminar();
-
-    // Create the fields
     private TextField seminarTitle = new TextField("Seminar-Titel");
-    private DatePicker seminarDate = new DatePicker("Zeitpunkt");
-    private Label infoLabel = new Label("Keine Info");
+    private Select<SeminarCategory> seminarCategory = new Select<SeminarCategory>();
+    private LocalDateTimePicker seminarDateTime = new LocalDateTimePicker("Zeitpunkt");
+    private TextArea seminarDescription = new TextArea("Beschreibung");
+    private TextField seminarLink = new TextField("Externer Link");
 
-    private NativeButton save = new NativeButton("Save");
-    private NativeButton reset = new NativeButton("Reset");
+    private TextField seminarStreet = new TextField("Strasse");
+    private TextField seminarStreetNbr = new TextField("Nr.");
+    private FormLayout streetComposite = new FormLayout(seminarStreet,seminarStreetNbr);
+
+    private NumberField seminarPlz = new NumberField ("PLZ");
+    private TextField seminarPlace = new TextField ("Ort");
+    private FormLayout placeComposite = new FormLayout(seminarPlace,seminarPlz);
+
+    private FormLayout seminarForm = new FormLayout();
 
 
+    private Label errorMessage = new Label("Hier könnte ihre Fehlermeldung stehen.");
 
+    //private NativeButton save = new NativeButton("Save");
+    private Button save = new Button("Save", new Icon(VaadinIcon.PLUS));
+    //private NativeButton reset = new NativeButton("Reset");
+    private Button reset = new Button("Reset", new Icon(VaadinIcon.ERASER));
+    private HorizontalLayout formActions = new HorizontalLayout(save,reset);
+
+
+    private Binder<Seminar> binder = new Binder<>();
+    private Seminar newSeminar = new Seminar();
     public NewSeminarView(){
+        seminarForm.add(seminarTitle);
+        seminarForm.add(seminarCategory);
+        seminarForm.add(streetComposite);
+        seminarForm.add(placeComposite);
+        seminarForm.add(seminarLink);
+        seminarForm.add(seminarDescription);
+
         seminarTitle.setRequiredIndicatorVisible(true);
-        seminarDate.setRequiredIndicatorVisible(true);
+        seminarCategory.setRequiredIndicatorVisible(true);
+        seminarDescription.setRequiredIndicatorVisible(true);
+        seminarLink.setRequiredIndicatorVisible(true);
+        seminarDateTime.setRequiredIndicatorVisible(true);
+        seminarPlz.setRequiredIndicatorVisible(true);
+        seminarStreet.setRequiredIndicatorVisible(true);
+        seminarPlace.setRequiredIndicatorVisible(true);
+        seminarStreetNbr.setRequiredIndicatorVisible(true);
+
+        seminarCategory.setLabel("Kategorie");
+        seminarCategory.setItems(SeminarCategoryManager.getSeminarCategories());
+        seminarCategory.setTextRenderer(SeminarCategory::getName);
+        seminarCategory.setItemLabelGenerator(SeminarCategory::getName);
 
         binder.bind(seminarTitle, Seminar::getTitle, Seminar::setTitle);
+        binder.bind(seminarDateTime, Seminar::getDate, Seminar::setDate);
+        binder.bind(seminarCategory, Seminar::getCategory, Seminar::setCategory);
+        binder.bind(seminarDescription, Seminar::getDescription, Seminar::setDescription);
+        binder.bind(seminarStreet, Seminar::getStreet, Seminar::setStreet);
+        binder.bind(seminarStreetNbr, Seminar::getHouseNumber, Seminar::setHouseNumber);
 
-        //binder.bind(seminarDate, Seminar::getDate, Seminar::setDate);
+        //binder.bind(seminarPlz, s->s.getPlz().doubleValue(), Seminar::setPlz);
+
+
 
         this.add(title);
-        this.add(seminarTitle);
-        this.add(seminarDate);
-        this.add(save);
-        this.add(reset);
+        this.add(seminarForm);
+        this.add(errorMessage);
+        this.add(formActions);
 
-        // Click listeners for the buttons
         save.addClickListener(event -> {
             if (binder.writeBeanIfValid(newSeminar)) {
                 SeminarManager.createSeminar(newSeminar);
@@ -63,13 +107,12 @@ public class NewSeminarView extends FormLayout {
                         .map(BindingValidationStatus::getMessage)
                         .map(Optional::get).distinct()
                         .collect(Collectors.joining(", "));
-                infoLabel.setText("There are errors: " + errorText);
+                errorMessage.setText("There are errors: " + errorText);
             }
         });
         reset.addClickListener(event -> {
-            // clear fields by setting null
             binder.readBean(null);
-            infoLabel.setText("");
+            errorMessage.setText("");
         });
     }
 }
