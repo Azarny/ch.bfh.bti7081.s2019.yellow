db5be1d92edb21d4dabc78bebe1ff71d7f864d3f
Xaltra\yaron <yaron.walter@gmail.com>
2019-05-13 23:28:31 +0200

    LocalDateTimePicket ist jetzt überflüssig.

--- src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
+++ src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
@@ -4,8 +4,8 @@ import ch.bfh.bti7081.model.manager.SeminarCategoryManager;
 import ch.bfh.bti7081.model.manager.SeminarManager;
 import ch.bfh.bti7081.model.seminar.Seminar;
 import ch.bfh.bti7081.model.seminar.SeminarCategory;
-import ch.bfh.bti7081.view.single_components.LocalDateTimePicker;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
@@ -17,11 +17,15 @@ import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.BinderValidationStatus;
 import com.vaadin.flow.data.binder.BindingValidationStatus;
 import com.vaadin.flow.router.Route;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -32,7 +36,11 @@ public class NewSeminarView extends VerticalLayout {
 
     private TextField seminarTitle = new TextField("Seminar-Titel");
     private Select<SeminarCategory> seminarCategory = new Select<SeminarCategory>();
-    private LocalDateTimePicker seminarDateTime = new LocalDateTimePicker("Zeitpunkt");
+
+    private TimePicker seminarTime = new TimePicker("Zeit");
+    private DatePicker seminarDate = new DatePicker("Datum");
+    private FormLayout dateComposite = new FormLayout(seminarDate,seminarTime);
+
     private TextArea seminarDescription = new TextArea("Beschreibung");
     private TextField seminarLink = new TextField("Externer Link");
 
@@ -42,10 +50,9 @@ public class NewSeminarView extends VerticalLayout {
 
     private NumberField seminarPlz = new NumberField("PLZ");
     private TextField seminarPlace = new TextField ("Ort");
-    private FormLayout placeComposite = new FormLayout(seminarPlace,seminarPlz);
+    private FormLayout placeComposite = new FormLayout(seminarPlz, seminarPlace);
 
     private FormLayout seminarForm = new FormLayout();
-
 
     private Label errorMessage = new Label("Hier könnte ihre Fehlermeldung stehen.");
 
@@ -60,16 +67,20 @@ public class NewSeminarView extends VerticalLayout {
 
     public NewSeminarView(){
         seminarForm.add(seminarTitle);
-        seminarForm.add(seminarDateTime);
         seminarForm.add(seminarCategory);
+
+        seminarForm.add(dateComposite);
+        seminarForm.add(seminarLink);
         seminarForm.add(streetComposite);
         seminarForm.add(placeComposite);
-        seminarForm.add(seminarLink);
+
         seminarForm.add(seminarDescription);
 
         seminarTitle.setRequiredIndicatorVisible(true);
-        seminarDateTime.setRequiredIndicatorVisible(true);
         seminarCategory.setRequiredIndicatorVisible(true);
+        //dateTimeComposite
+        seminarDate.setRequiredIndicatorVisible(true);
+        seminarTime.setRequiredIndicatorVisible(true);
         //streetComposite
         seminarStreet.setRequiredIndicatorVisible(true);
         seminarStreetNbr.setRequiredIndicatorVisible(true);
@@ -86,17 +97,32 @@ public class NewSeminarView extends VerticalLayout {
         seminarCategory.setItemLabelGenerator(SeminarCategory::getName);
         seminarCategory.setEmptySelectionAllowed(false);
 
-
         //Binder-Configuration
         binder.bind(seminarTitle, Seminar::getTitle, Seminar::setTitle);
-        binder.bind(seminarDateTime, Seminar::getDate, Seminar::setDate);
-        //Kategorie ist notwendig
+
+        //DateTimeConvertation
+        //withConverter(UI-Value to Model, Model-Value to UI, Error Message if not succesfull)
+        //When Time or Date are saved, the other component is read to fill the datetime.
+        binder.forField(seminarDate).withConverter(date->{
+            LocalTime time = seminarTime.getValue();
+            if(time==null){
+                time = LocalTime.of(0,0);
+            }
+            return LocalDateTime.of(date, time);
+        }, LocalDateTime::toLocalDate).
+                bind(Seminar::getDate,Seminar::setDate);
+
+        binder.forField(seminarTime).withConverter(time->{
+            LocalDate date = seminarDate.getValue();
+            return LocalDateTime.of(date, time);
+        },LocalDateTime::toLocalTime).
+                bind(Seminar::getDate,Seminar::setDate);
+
         binder.forField(seminarCategory).asRequired("Bitte eine Kategorie wählen.").
                 bind(Seminar::getCategory, Seminar::setCategory);
         binder.bind(seminarStreet, Seminar::getStreet, Seminar::setStreet);
         binder.bind(seminarStreetNbr, Seminar::getHouseNumber, Seminar::setHouseNumber);
         binder.forField(seminarPlz).
-                //withConverter(UI-Value to Model, Model-Value to UI, Error Message if not succesfull)
                 withConverter(Double::intValue, Integer::doubleValue,"Bitte eine PLZ eingeben").
                 bind(Seminar::getPlz, Seminar::setPlz);
         binder.bind(seminarPlace, Seminar::getLocation, Seminar::setLocation);
