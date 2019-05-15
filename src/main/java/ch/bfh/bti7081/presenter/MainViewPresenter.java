145f0a6cf8dee78767c4c449cf504f7bb7d99f24
Lucien Heuzeveldt <lucien.heuzeveldt@gmail.com>
2019-05-15 20:03:56 +0200

    move Vaadin part of next seminaries into MainViewContent

--- src/main/java/ch/bfh/bti7081/presenter/MainViewPresenter.java
+++ src/main/java/ch/bfh/bti7081/presenter/MainViewPresenter.java
@@ -3,26 +3,25 @@ package ch.bfh.bti7081.presenter;
 import ch.bfh.bti7081.model.manager.SeminarManager;
 import ch.bfh.bti7081.model.seminar.Seminar;
 import ch.bfh.bti7081.view.MainViewContent;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
+
 public class MainViewPresenter {
     public MainViewPresenter(MainViewContent view) {
         // generate next seminars on startup
-        view.setFeatureView(this.getNextSeminaries());
+        view.setFeatureView(getNextSeminaries());
     }
 
     /**
      * creates a VerticalLayout with a title and the next few seminaries to display on the homepage
+     *
      * @return VerticalLayout to add to view
      */
-    private VerticalLayout getNextSeminaries() {
-        VerticalLayout nextSeminars = new VerticalLayout(new H2("Nächste Seminare"));
+    private static List<String> getNextSeminaries() {
+        List<String> nextSeminars = new ArrayList<>();
         List<Seminar> seminaries = SeminarManager.getSeminaries();
         seminaries.sort(Comparator.comparing(Seminar::getDate));
         LocalDateTime now = LocalDateTime.now();
@@ -37,9 +36,9 @@ public class MainViewPresenter {
 
         // collect the necessary information from the seminaries and prepare them for display
         for (Seminar seminar : seminaries) {
-            nextSeminars.add(new Label(
+            nextSeminars.add(
                     seminar.getDate().format(dateTimeFormatter) + " - " +
-                            seminar.getTitle() + " (" + seminar.getLocation() + ")")
+                            seminar.getTitle() + " (" + seminar.getLocation() + ")"
             );
         }
 
--- src/main/java/ch/bfh/bti7081/view/MainViewContent.java
+++ src/main/java/ch/bfh/bti7081/view/MainViewContent.java
@@ -6,8 +6,13 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
+import java.util.List;
+
 public class MainViewContent extends VerticalLayout {
-    private String loremipsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    private String loremipsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
+            "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, " +
+            "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " +
+            "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
     public MainViewContent() {
         // title and welcometext
@@ -15,14 +20,21 @@ public class MainViewContent extends VerticalLayout {
         this.add(new Label(loremipsum));
     }
 
-    public void setFeatureView(VerticalLayout seminariesLayout) {
+    public void setFeatureView(List<String> seminaries) {
         // block "Wichtige Links"
         H2 linksTitle = new H2("Wichtige Links");
         Label linksText = new Label(loremipsum);
         VerticalLayout linksLayout = new VerticalLayout(linksTitle, linksText);
 
         // block "Hot Topics"
-         VerticalLayout hotTopicsLayout = new VerticalLayout(new Label(loremipsum));
+        VerticalLayout hotTopicsLayout = new VerticalLayout(new Label(loremipsum));
+
+        // block "nächste Seminare"
+        VerticalLayout seminariesLayout = new VerticalLayout();
+        seminariesLayout.add(new H2("Nächste Seminare"));
+        for (String seminar : seminaries) {
+            seminariesLayout.add(new Label(seminar));
+        }
 
         // combine the feature-views and add them to site
         HorizontalLayout siteFeatures = new HorizontalLayout(linksLayout, seminariesLayout, hotTopicsLayout);
