893ea047e4abc07daac13852c450e67af51a8b0f
Miishale <mischae@bluewin.ch>
2019-05-16 10:56:27 +0200

    frontend validation + removal of magic numbers

--- src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
+++ src/main/java/ch/bfh/bti7081/view/NewSeminarView.java
@@ -1,8 +1,26 @@
 package ch.bfh.bti7081.view;
 
-import ch.bfh.bti7081.presenter.NewSeminarPresenter;
-
+import ch.bfh.bti7081.model.manager.SeminarCategoryManager;
+import ch.bfh.bti7081.model.manager.SeminarManager;
+import ch.bfh.bti7081.model.seminar.Seminar;
+import ch.bfh.bti7081.model.seminar.SeminarCategory;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.BinderValidationStatus;
+import com.vaadin.flow.data.binder.BindingValidationStatus;
 import com.vaadin.flow.router.Route;
 
 import java.time.LocalDate;
@@ -17,7 +35,7 @@ public class NewSeminarView extends VerticalLayout {
     private H1 title = new H1("Seminar erstellen");
 
     private TextField seminarTitle = new TextField("Seminar-Titel");
-    private Select<SeminarCategory> seminarCategory = new Select<SeminarCategory>();
+    private Select<SeminarCategory> seminarCategory = new Select<>();
 
     private TimePicker seminarTime = new TimePicker("Zeit");
     private DatePicker seminarDate = new DatePicker("Datum");
@@ -48,9 +66,13 @@ public class NewSeminarView extends VerticalLayout {
     private Seminar newSeminar = new Seminar();
 
     public NewSeminarView(){
-        NewSeminarViewImpl view = new NewSeminarViewImpl();
-        NewSeminarPresenter presenter = new NewSeminarPresenter(view);
-        view.setPresenter(presenter);
+        seminarForm.add(seminarTitle);
+        seminarForm.add(seminarCategory);
+
+        seminarForm.add(dateComposite);
+        seminarForm.add(seminarLink);
+        seminarForm.add(streetComposite);
+        seminarForm.add(placeComposite);
 
         seminarForm.add(seminarDescription);
 
@@ -76,36 +98,64 @@ public class NewSeminarView extends VerticalLayout {
         seminarCategory.setEmptySelectionAllowed(false);
 
         //Binder-Configuration
-        binder.bind(seminarTitle, Seminar::getTitle, Seminar::setTitle);
+        binder.forField(seminarTitle).asRequired("Bitte Titel angeben.").
+                withValidator(title -> title.trim().length() >= SeminarManager.minTitleLength, "Titel muss aus mind. "+SeminarManager.minTitleLength + " Zeichen bestehen.").
+                bind(Seminar::getTitle, Seminar::setTitle);
 
         //DateTimeConvertation
         //withConverter(UI-Value to Model, Model-Value to UI, Error Message if not succesfull)
         //When Time or Date are saved, the other component is read to fill the datetime.
-        binder.forField(seminarDate).withConverter(date->{
-            LocalTime time = seminarTime.getValue();
-            if(time==null){
-                time = LocalTime.of(0,0);
-            }
-            return LocalDateTime.of(date, time);
-        }, LocalDateTime::toLocalDate).
+        binder.forField(seminarDate).asRequired("Bitte Datum angeben.").
+                withConverter(date->{
+                    LocalTime time = seminarTime.getValue();
+                    if(time==null){
+                        time = LocalTime.of(0,0);
+                    }
+                    return LocalDateTime.of(date, time);
+                }, LocalDateTime::toLocalDate)
+                .withValidator(seminarDate -> seminarDate.isAfter(LocalDateTime.now().minusDays(1)), "Datum ist in der Vergangenheit.")
+                .withValidator(seminarDate -> seminarDate.isBefore(LocalDateTime.now().plusYears(SeminarManager.maxYearsInFuture)), "Datum ist mehr als " + SeminarManager.maxYearsInFuture + " Jahre in der Zukunft.")
+                .bind(Seminar::getDate,Seminar::setDate);
+
+        binder.forField(seminarTime).asRequired("Bitte Zeit angeben.").
+                withConverter(time->{
+                    LocalDate date = seminarDate.getValue();
+                    return LocalDateTime.of(date, time);
+                },LocalDateTime::toLocalTime).
                 bind(Seminar::getDate,Seminar::setDate);
 
-        binder.forField(seminarTime).withConverter(time->{
-            LocalDate date = seminarDate.getValue();
-            return LocalDateTime.of(date, time);
-        },LocalDateTime::toLocalTime).
-                bind(Seminar::getDate,Seminar::setDate);
-
-        binder.forField(seminarCategory).asRequired("Bitte eine Kategorie wählen.").
+        binder.forField(seminarCategory).
+                asRequired("Bitte eine Kategorie wählen.").
                 bind(Seminar::getCategory, Seminar::setCategory);
-        binder.bind(seminarStreet, Seminar::getStreet, Seminar::setStreet);
-        binder.bind(seminarStreetNbr, Seminar::getHouseNumber, Seminar::setHouseNumber);
+
+        binder.forField(seminarStreet).asRequired("Bitte Strassennamen angeben.").
+                withValidator(street -> street.trim().length() >= SeminarManager.minStreetLength, "Strasse muss aus mind. " + SeminarManager.minStreetLength + " Zeichen bestehen.").
+                bind(Seminar::getStreet, Seminar::setStreet);
+
+        binder.forField(seminarStreetNbr).
+                asRequired("Bitte Hausnummer angeben.").
+                withValidator(houseNumber -> houseNumber.matches("^\\d*\\w$"), "Hausnummer ungültig.").
+                withValidator(houseNumber -> houseNumber.trim().length() >= SeminarManager.minStreetNumberLength, "Hausnummer muss aus mind. " + SeminarManager.minStreetNumberLength + "bestehen.").
+                bind(Seminar::getHouseNumber, Seminar::setHouseNumber);
+
         binder.forField(seminarPlz).
                 withConverter(Double::intValue, Integer::doubleValue,"Bitte eine PLZ eingeben").
+                withValidator(plz -> ((plz > 999 && plz < 10000) || (plz > 99999 && plz < 1000000)), "Ungültige PLZ.").
                 bind(Seminar::getPlz, Seminar::setPlz);
-        binder.bind(seminarPlace, Seminar::getLocation, Seminar::setLocation);
-        binder.bind(seminarLink, Seminar::getLink, Seminar::setLink);
-        binder.bind(seminarDescription, Seminar::getDescription, Seminar::setDescription);
+
+        binder.forField(seminarPlace).asRequired("Bitte Ort angeben.").
+                withValidator(location -> location.length() >= SeminarManager.minLocationLength, "Ort muss aus mind. " + SeminarManager.minLocationLength +" Zeichen bestehen.").
+                bind(Seminar::getLocation, Seminar::setLocation);
+
+        binder.forField(seminarLink).
+                asRequired("Bitte URL angeben.").
+                withValidator(url -> url.matches("^((https?)://)?(\\w+\\.)+(\\w{2}|\\w{3})(/\\S+(\\./\\S+)*)?$"),"Ungültiger Link").
+                bind(Seminar::getUrl, Seminar::setUrl);
+
+        binder.forField(seminarDescription).
+                asRequired("Bitte Beschreibung angeben").
+                withValidator(description -> description.trim().length() >= SeminarManager.minDescriptionLength, "Beschreibung muss aus mind. " + SeminarManager.minDescriptionLength + " Zeichen bestehen.").
+                bind(Seminar::getDescription, Seminar::setDescription);
 
         this.add(title);
         this.add(seminarForm);
@@ -128,8 +178,6 @@ public class NewSeminarView extends VerticalLayout {
             }
         });
 
-        cancel.addClickListener(event -> {
-            cancel.getUI().ifPresent(ui -> ui.navigate("seminar"));
-        });
+        cancel.addClickListener(event -> cancel.getUI().ifPresent(ui -> ui.navigate("seminar")));
     }
 }
