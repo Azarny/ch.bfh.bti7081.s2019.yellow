0b2743460b6936ca0ce95cdc118c7834110f1a52
Miishale <mischae@bluewin.ch>
2019-05-31 12:52:10 +0200

    Speichern umgesetzt

--- src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
+++ src/main/java/ch/bfh/bti7081/model/manager/SeminarManager.java
@@ -22,7 +22,7 @@ public class SeminarManager {
     private SeminarRepository seminarRepository;
 
     public List<Seminar> getSeminaries() {
-        return mockSeminaries();
+        return seminarRepository.findAll();
     }
 
     public List<Seminar> getFilteredSeminars(SeminarFilter filter) {
@@ -139,68 +139,6 @@ public class SeminarManager {
         }
 
         return returnString;
-    }
-
-    private List<Seminar> mockSeminaries() {
-        //These is mock data, nothing here is productive code.
-        List<Seminar> mockSeminaries = new ArrayList<>();
-        SeminarCategoryManager categoryManager = new SeminarCategoryManager();
-
-        Seminar seminarMock1 = new Seminar();
-        seminarMock1.setCategory(categoryManager.getSeminarCategories().get(2));
-        seminarMock1.setDate(dateGenerator("2019-11-07 13:30"));
-        seminarMock1.setDescription("Ärzte berichten von ihren Erlebnissen mit der Krankheit. " +
-                "Dr. Luis Alvador gibt Ihnen einen Überblick über die medizinischen Informationen, " +
-                "eine Podiumsdiskussion mit Angehörigen findet statt.");
-        seminarMock1.setHouseNumber("102");
-        seminarMock1.setUrl("https://vaadin.com");
-        seminarMock1.setLocation("Bern");
-        seminarMock1.setTitle("Wie unterstütze ich einen Angehörigen?");
-        seminarMock1.setStreet("Wankdorffeldstrasse");
-        seminarMock1.setPlz(3014);
-        mockSeminaries.add(seminarMock1);
-
-        Seminar seminarMock2 = new Seminar();
-        seminarMock2.setCategory(categoryManager.getSeminarCategories().get(0));
-        seminarMock2.setDate(dateGenerator("2019-08-07 10:30"));
-        seminarMock2.setDescription("Betroffene erzählen von Ihren Problemen im Umgang mit der Krankheit " +
-                "und zeigen wie Sie diese tagtäglich überwinden.");
-        seminarMock2.setHouseNumber("5");
-        seminarMock2.setUrl("https://www.nzz.ch/");
-        seminarMock2.setLocation("Bern");
-        seminarMock2.setTitle("Sozialphobie - Mit der Angst umgehen");
-        seminarMock2.setStreet(" Schanzenstrasse");
-        seminarMock2.setPlz(3008);
-        mockSeminaries.add(seminarMock2);
-
-        Seminar seminarMock3 = new Seminar();
-        seminarMock3.setCategory(categoryManager.getSeminarCategories().get(1));
-        seminarMock3.setDate(dateGenerator("2019-09-09 20:00"));
-        seminarMock3.setDescription("Wir haben zehn Angehörige von Personen mit Sozialphobie eingeladen, " +
-                "die Ihnen von Ihren Erlebnissen erzählen. Treffen Sie uns zu einem Feierabendbier. " +
-                "Mehr unter dem Link. ");
-        seminarMock3.setHouseNumber("18");
-        seminarMock3.setUrl("https://www.zeit.de/index");
-        seminarMock3.setLocation("Rothrist");
-        seminarMock3.setTitle("Angehörigentreffen");
-        seminarMock3.setStreet("Bachweg");
-        seminarMock3.setPlz(4852);
-        mockSeminaries.add(seminarMock3);
-
-        Seminar seminarMock4 = new Seminar();
-        seminarMock4.setCategory(categoryManager.getSeminarCategories().get(0));
-        seminarMock4.setDate(dateGenerator("2020-02-15 09:00"));
-        seminarMock4.setDescription("Ein Treffen mit anderen Betroffenen. Hier können Sie sich entspannt " +
-                "fühlen, niemand wird sie verurteilen, da wir dasselbe tagtäglich auch durchmachen.");
-        seminarMock4.setHouseNumber("420");
-        seminarMock4.setUrl("https://de.wikipedia.org/wiki/Soziale_Phobie");
-        seminarMock4.setLocation("Zürich");
-        seminarMock4.setTitle("Betroffenenhöck");
-        seminarMock4.setStreet("Badenerstrasse");
-        seminarMock4.setPlz(8040);
-        mockSeminaries.add(seminarMock4);
-
-        return mockSeminaries;
     }
 
     private LocalDateTime dateGenerator(String timeToParse) {
--- src/main/java/ch/bfh/bti7081/model/manager/UserManager.java
+++ src/main/java/ch/bfh/bti7081/model/manager/UserManager.java
@@ -1,16 +1,20 @@
 package ch.bfh.bti7081.model.manager;
 
 import ch.bfh.bti7081.model.User;
+import ch.bfh.bti7081.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Component
 public class UserManager {
+
+    @Autowired
+    UserRepository userRepository;
+
     public User getUserByUsername(String name) {
-        Optional<User> user = getMockUsers().stream().filter(u -> u.getUsername().equals(name)).findAny();
+        Optional<User> user = userRepository.findAll().stream().filter(u -> u.getUsername().equals(name)).findAny();
         if (user.isPresent()) {
             return user.get();
         } else {
@@ -19,29 +23,12 @@ public class UserManager {
 
     }
 
+    public void createUser(User user){
+        userRepository.save(user);
+    }
+
     public String encryptPassword(String password){
         return "Not implemented yet";
     }
-
-
-
-    private static List<User> getMockUsers() {
-        //These is mock data, nothing here is productive code.
-        List<User> mockUsers = new ArrayList<>();
-        User mockUser1 = new User();
-        mockUser1.setEmail("nomail@nomailhausen.com");
-        mockUser1.setPassword("12345");
-        mockUser1.setPermission(1);
-        mockUser1.setUsername("Lara");
-        mockUsers.add(mockUser1);
-        User mockUser2 = new User();
-        mockUser2.setEmail("nomail@nomailhausen.com");
-        mockUser2.setPassword("12345");
-        mockUser2.setPermission(4);
-        mockUser2.setUsername("Admin");
-        mockUsers.add(mockUser2);
-        return mockUsers;
-    }
-
 
 }
